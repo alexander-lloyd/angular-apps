8a4d89bbbff7a2487ae93a6dbdd88e0b62f3e216
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-03-22 18:17:55 +0000

    feat(logger): provide log service in log module

--- libs/logger/src/lib/logger.module.ts
+++ libs/logger/src/lib/logger.module.ts
@@ -1,12 +1,19 @@
 import {NgModule, ValueProvider} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {LOG_HANDLERS_TOKEN} from './api/token';
+
+import {LOG_HANDLERS_TOKEN, LoggerService, LOG_SERVICE_TOKEN} from './api';
 import {ConsoleHandler} from './impl';
+import {LoggerServiceImpl} from './logger.service';
 
 const logProviders: ValueProvider = {
   provide: LOG_HANDLERS_TOKEN,
   useValue: ConsoleHandler,
   multi: true
+};
+
+const logServiceProvider: ValueProvider = {
+  provide: LOG_SERVICE_TOKEN,
+  useValue: LoggerServiceImpl
 };
 
 /**
@@ -15,7 +22,8 @@ const logProviders: ValueProvider = {
 @NgModule({
   imports: [CommonModule],
   providers: [
-    logProviders
+    logProviders,
+    logServiceProvider
   ]
 })
 export class LoggerModule {}
