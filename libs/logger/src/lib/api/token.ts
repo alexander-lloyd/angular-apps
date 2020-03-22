0eac7dd0f22ebc98486e9489a4824aa402da0460
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-03-22 18:17:55 +0000

    feat(logger): provide log service in log module

--- libs/logger/src/lib/api/token.ts
+++ libs/logger/src/lib/api/token.ts
@@ -3,3 +3,5 @@ import {InjectionToken} from '@angular/core';
 import {LogHandler} from './handler';
 
 export const LOG_HANDLERS_TOKEN = new InjectionToken<LogHandler[]>('LOG_HANDLERS_TOKEN');
+
+export const LOG_SERVICE_TOKEN = new InjectionToken('LOG_SERVICE_TOKEN');
