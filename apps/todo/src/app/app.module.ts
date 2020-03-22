6299964d1eb740cb6b86cdeaa8fe384f658e55a8
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-03-22 18:56:15 +0000

    feat(todo): Add logger module

--- apps/todo/src/app/app.module.ts
+++ apps/todo/src/app/app.module.ts
@@ -2,13 +2,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {LoggerModule} from '@al/logger';
 
 /**
  * AppModule.
  */
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, LoggerModule],
   providers: [],
   bootstrap: [AppComponent]
 })
