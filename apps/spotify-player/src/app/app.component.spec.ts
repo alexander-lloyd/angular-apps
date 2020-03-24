dbb43558a051b7d04142cbdadb87ee97984a3185
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-03-24 15:56:19 +0000

    feat(spotify-player): log into spotify works

--- apps/spotify-player/src/app/app.component.spec.ts
+++ apps/spotify-player/src/app/app.component.spec.ts
@@ -1,11 +1,32 @@
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {TestBed, async} from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing'
+import {OAuthService, AuthConfig} from 'angular-oauth2-oidc';
+
 import {AppComponent} from './app.component';
+
+class OAuthServiceStub {
+  public configure(config: AuthConfig): void {
+  }
+
+  public tryLogin(): Promise<boolean> {
+    return Promise.resolve(true);
+  }
+
+  public initImplicitFlow() {
+  }
+}
+
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [HttpClientTestingModule],
+      providers: [{
+        provide: OAuthService,
+        useClass: OAuthServiceStub
+      }],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
---
+++ apps/spotify-player/src/app/app.config.ts
@@ -0,0 +1,11 @@
+import {AuthConfig} from 'angular-oauth2-oidc';
+
+export const authConfig: AuthConfig = {
+  loginUrl: 'https://accounts.spotify.com/authorize',
+  redirectUri: `${window.location.origin}/index.html`,
+  clientId: '67ece50079eb4ff5baf9969e6905f022',
+  responseType: 'token',
+  scope: 'user-modify-playback-state',
+  showDebugInformation: true,
+  oidc: false
+};
