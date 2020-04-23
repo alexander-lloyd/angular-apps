62c4f472257fe13a8853eaeaa665387d5eb6758c
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-04-23 18:11:47 +0100

    feat(spotify-player): remove material icons and use font awesome instead

--- apps/spotify-player/src/app/media-control/media-control.component.ts
+++ apps/spotify-player/src/app/media-control/media-control.component.ts
@@ -1,4 +1,10 @@
 import {Component, ChangeDetectionStrategy} from '@angular/core';
+import {
+  faPause,
+  faPlay,
+  faStepBackward,
+  faStepForward
+} from '@fortawesome/free-solid-svg-icons';
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs';
 
@@ -11,6 +17,10 @@ import {SpotifyState, actions, selectors} from './store';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MediaControlComponent {
+  public faPause = faPause;
+  public faPlay = faPlay;
+  public faStepBackward = faStepBackward;
+  public faStepForward = faStepForward;
   public isPlaying$: Observable<boolean>;
 
   public constructor(
--- apps/spotify-player/src/app/media-control/media-control.module.ts
+++ apps/spotify-player/src/app/media-control/media-control.module.ts
@@ -1,16 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 import {MediaControlComponent} from './media-control.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    FontAwesomeModule,
     MatButtonModule,
-    MatIconModule,
     MatProgressBarModule
   ],
   exports: [MediaControlComponent],
