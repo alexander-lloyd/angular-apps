52370ea5c6385a6ab1f3c1721619b5083d869fd9
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-05-03 16:10:37 +0100

    feat(todo): add toolbar and side menu

--- apps/todo/src/app/app.module.ts
+++ apps/todo/src/app/app.module.ts
@@ -1,15 +1,30 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
-import {AppComponent} from './app.component';
 import {LoggerModule} from '@al/logger';
+import {AppComponent} from './app.component';
 
 /**
  * AppModule.
  */
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, LoggerModule],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FontAwesomeModule,
+    LoggerModule,
+    MatButtonModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
