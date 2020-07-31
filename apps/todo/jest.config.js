b531a3e098432642217b71e14f976a59cb739e86
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-07-31 13:15:05 +0100

    fix(todo): add missing jest config

--- apps/todo/jest.config.js
+++ apps/todo/jest.config.js
@@ -1,10 +1,22 @@
+/* eslint-env node */
 module.exports = {
-  name: 'todo',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/todo',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js'
-  ]
+    name: 'todo',
+    preset: '../../jest.config.js',
+    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+    globals: {
+        'ts-jest': {
+            tsConfig: '<rootDir>/tsconfig.spec.json',
+            stringifyContentPathRegex: '\\.(html|svg)$',
+            astTransformers: [
+                'jest-preset-angular/build/InlineFilesTransformer',
+                'jest-preset-angular/build/StripStylesTransformer'
+            ]
+        }
+    },
+    coverageDirectory: '../../coverage/apps/todo',
+    snapshotSerializers: [
+        'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+        'jest-preset-angular/build/AngularSnapshotSerializer.js',
+        'jest-preset-angular/build/HTMLCommentSerializer.js'
+    ]
 };
