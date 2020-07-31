ff3811bff1046e9a91ba1433e1cdde8a010234ee
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-07-31 21:33:01 +0100

    feat(button): add button component story

---
+++ libs/components/button/src/lib/button.component.stories.ts
@@ -0,0 +1,16 @@
+
+import {ButtonComponent} from './button.component';
+
+export default {
+  title: 'Button Component'
+};
+
+
+export const primary = () => ({
+  moduleMetadata: {
+    imports: []
+  },
+  component: ButtonComponent,
+  props: {
+  }
+});
