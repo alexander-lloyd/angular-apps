df6be5f8b7cc2732bd183c68a624dfb85ca3dda8
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-08-08 15:46:17 +0100

    fix(lint): fix linting in button-component

--- apps/components/button-component-e2e/src/plugins/index.js
+++ apps/components/button-component-e2e/src/plugins/index.js
@@ -1,22 +1,26 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+/* eslint-env node */
+/*
+ * This example plugins/index.js can be used to load plugins
+ *
+ * You can change the location of this file or turn off loading
+ * the plugins file with the 'pluginsFile' configuration option.
+ *
+ * You can read more here:
+ * https://on.cypress.io/plugins-guide
+ */
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+/*
+ * This function is called when a project is opened or re-opened (e.g. due to
+ * the project's config changing)
+ */
 
-const { preprocessTypescript } = require('@nrwl/cypress/plugins/preprocessor');
+const {preprocessTypescript} = require('@nrwl/cypress/plugins/preprocessor');
 
+/**
+ * @param on is used to hook into various events Cypress emits
+ * @param config is the resolved Cypress config
+ */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-
-  // Preprocess Typescript file using Nx helper
-  on('file:preprocessor', preprocessTypescript(config));
+    // Preprocess Typescript file using Nx helper
+    on('file:preprocessor', preprocessTypescript(config));
 };
