1aa90bce1badb34fe043f67b688f96223368bc14
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-08-08 14:16:13 +0100

    fix(button): add missing exports

--- libs/components/button/src/index.ts
+++ libs/components/button/src/index.ts
@@ -1,1 +1,3 @@
-export * from './lib/button.module';
+export {ButtonModule} from './lib/button.module';
+export {ButtonComponent} from './lib/button.component';
+export {ButtonHarness, ButtonHarnessFilters} from './lib/button-harness';
