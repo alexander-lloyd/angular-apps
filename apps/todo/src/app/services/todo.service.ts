d78f7e05a791ec32fd060f50e6ac3f32ff21d88a
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-08-27 22:09:40 +0100

    feat(todo): add todo service

--- apps/todo/src/app/services/todo.service.ts
+++ apps/todo/src/app/services/todo.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Observable, of} from 'rxjs';
 
+import {LocalStorageService} from './local-storage.service';
 import {TodoTask} from '../types/todo.types';
 
 /**
@@ -11,11 +12,22 @@ export class TodoService {
   public static readonly TODOS_KEY = 'todos';
 
   /**
+   * Constructor.
+   *
+   * @param localStorage LocalStorageService.
+   */
+  public constructor(
+    private localStorage: LocalStorageService
+  ) {}
+
+  /**
    * Get all of the todos.
    *
    * @returns List of todo objects.
    */
   public getTodos(): Observable<TodoTask[]> {
-    return of([]);
+    const todosJson = this.localStorage.getItem(TodoService.TODOS_KEY);
+    const todos = JSON.parse(todosJson) as TodoTask[];
+    return of(todos);
   }
 }
