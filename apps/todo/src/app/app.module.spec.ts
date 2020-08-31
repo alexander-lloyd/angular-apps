01762368abd2225bce936c9d31ae65de73665a3a
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-08-31 16:21:24 +0100

    feat(todo): add test for app module

---
+++ apps/todo/src/app/app.module.spec.ts
@@ -0,0 +1,8 @@
+import {AppModule} from './app.module';
+
+describe('AppModule', () => {
+  it('should be truthy', () => {
+    expect.assertions(1);
+    expect(AppModule).toBeTruthy();
+  });
+});
