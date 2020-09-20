f51ceda819a73f2445d1c3a39d6b1ff8fa0286f3
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-09-20 20:26:18 +0100

    fix(todo): fix e2e test

--- apps/todo-e2e/src/support/app.po.ts
+++ apps/todo-e2e/src/support/app.po.ts
@@ -1,6 +1,20 @@
 /**
- * Get the greeting.
+ * Get the title bar.
  *
  * @returns Chainable.
  */
-export const getGreeting = (): Cypress.Chainable => cy.get('h1');
+export const getTitleBar = (): Cypress.Chainable => cy.get('mat-toolbar');
+
+/**
+ * Get the side nav.
+ *
+ * @returns Chainable.
+ */
+export const getMainContent = (): Cypress.Chainable => cy.get('mat-sidenav-content');
+
+/**
+ * Get the create todo form.
+ *
+ * @returns Chainable.
+ */
+export const getCreateTodoForm = (): Cypress.Chainable => getMainContent().get('al-create-todo');
