25e6e90ad3c334770adf74e2f3286855f2eb9b6c
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-09-20 20:26:18 +0100

    fix(todo): fix e2e test

--- apps/todo-e2e/src/integration/app.spec.ts
+++ apps/todo-e2e/src/integration/app.spec.ts
@@ -1,13 +1,13 @@
-import {getGreeting} from '../support/app.po';
+import {getCreateTodoForm, getTitleBar} from '../support/app.po';
 
 describe('todo', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should have title bar', () => {
+    getTitleBar().contains('Todo App');
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to todo!');
+  it('should have create todo form', () => {
+    getCreateTodoForm().should('have.length', 1);
   });
 });
