16406a2ecbf0bec9dfacebe74a6419f8f2856353
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-10-03 22:47:30 +0100

    chore: upgrade nrwl

--- libs/logger/jest.config.js
+++ libs/logger/jest.config.js
@@ -1,12 +1,13 @@
 /* eslint-env node */
 
 module.exports = {
-  name: 'logger',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/logger',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js'
-  ]
+    name: 'logger',
+    preset: '../../jest.config.js',
+    coverageDirectory: '../../coverage/libs/logger',
+    snapshotSerializers: [
+        'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+        'jest-preset-angular/build/AngularSnapshotSerializer.js',
+        'jest-preset-angular/build/HTMLCommentSerializer.js'
+    ],
+    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts']
 };
