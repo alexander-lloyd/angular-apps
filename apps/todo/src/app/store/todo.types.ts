924b15704a648a5cc2410c2b5ba730e2cd3c1107
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-10-03 22:48:48 +0100

    feat(todo): clicking checkbox sends event

--- apps/todo/src/app/store/todo.types.ts
+++ apps/todo/src/app/store/todo.types.ts
@@ -1,17 +1,29 @@
 import {TodoTask} from '../types/todo.types';
 
+/**
+ * TodoStore Type.
+ */
 export interface TodoStore {
   todos: TodoTask[];
 }
 
+/**
+ * GlobalStore Type.
+ */
 export interface GlobalState {
   todo: TodoStore;
 }
 
+/**
+ * Add Todo Action Properties.
+ */
 export interface AddTodoActionProperties {
   task: TodoTask;
 }
 
+/**
+ * Get Todo Success Properties.
+ */
 export interface GetTodoSuccessProperties {
   todos: TodoTask[];
 }
