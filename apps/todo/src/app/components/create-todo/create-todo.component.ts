b9b2341449561a90f22f024b1d8802342f63de09
Alexander Lloyd <axl639@student.bham.ac.uk>
2020-10-05 23:20:35 +0100

    feat(todo): entering a date into input field will set todo due date

--- apps/todo/src/app/components/create-todo/create-todo.component.ts
+++ apps/todo/src/app/components/create-todo/create-todo.component.ts
@@ -1,6 +1,13 @@
+import {SPACE} from '@angular/cdk/keycodes';
 import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import {MatChipInputEvent} from '@angular/material/chips';
 import {v4 as uuidv4} from 'uuid';
 import {TodoTask} from '../../types/todo.types';
+
+interface CreateTodoFormInput {
+  name: string;
+  due: number;
+}
 
 /**
  * Create Todo Form Component.
@@ -11,7 +18,12 @@ import {TodoTask} from '../../types/todo.types';
   styleUrls: ['./create-todo.component.scss']
 })
 export class CreateTodoComponent {
-  public value = '';
+  public readonly separatorKeysCodes: number[] = [SPACE];
+
+  public task: CreateTodoFormInput = {
+    due: 0,
+    name: ''
+  };
 
   @Output()
   public submitTodo = new EventEmitter<TodoTask>();
@@ -22,18 +34,61 @@ export class CreateTodoComponent {
   /**
    * Internal method called when form is submitted.
    * Calls the EventEmitter.
-   *
-   * @param name Name of task.
    */
-  public _onSubmit(name: string): void {
+  public _onSubmit(): void {
     const task: TodoTask = {
-      completed: false,
-      due: 0,
+      ...this.task,
       id: uuidv4(),
-      name
+      completed: false
     };
+
     this.submitTodo.emit(task);
     // Clear the input
-    this.value = '';
+    this.task.name = '';
+    this.task.due = 0;
+  }
+
+  /**
+   * Add a chip.
+   *
+   * @param event MatChipEvent. The value of the input.
+   */
+  public add(event: MatChipInputEvent): void {
+    const parsedInput = this.parseDate(event.value);
+    this.task = {
+      due: parsedInput.due,
+      name: `${parsedInput.name} `
+    };
+  }
+
+  /**
+   * Remove the chip.
+   */
+  public remove(): void {
+    this.task.due = 0;
+  }
+
+  /**
+   * Parse the date from the form input.
+   *
+   * @param input Raw Input.
+   * @returns Parsed Input.
+   */
+  private parseDate(input: string): CreateTodoFormInput {
+    let parsedDate = 0;
+    const parsedInput = input.split(String.fromCharCode(SPACE))
+      .map((word) => {
+        if (word.match(/\d{4}-\d{2}-\d{2}/u) !== null) {
+          parsedDate = Date.parse(word);
+          // Remove the word;
+          return '';
+        }
+        return word;
+      });
+
+    return {
+      name: parsedInput.join(' '),
+      due: parsedDate
+    };
   }
 }
