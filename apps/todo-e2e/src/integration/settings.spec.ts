612fb1d1540597001d2129bf6d5e19b0aa53148e
Alexander Lloyd <axl639@student.bham.ac.uk>
2021-01-24 13:03:27 +0000

    feat(todo): add e2e tests and clean up config files

---
+++ apps/todo-e2e/src/integration/settings.spec.ts
@@ -0,0 +1,37 @@
+import * as selectors from '../support/app.po';
+
+describe('Settings Dialog', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display a dialog', () => {
+    selectors.getSettingsDialog().should('not.exist');
+    selectors.getSettingsButton().click();
+    selectors.getSettingsDialog().should('exist');
+  });
+
+  it('should change language', () => {
+    selectors.getLanguage().should('eq', 'en');
+
+    selectors.getSettingsButton().click();
+    selectors.getLanguageSelect().click()
+      .get('mat-option')
+      .contains('French')
+      .click();
+    selectors.getSaveSettingsButton().click();
+
+    selectors.getLanguage().should('eq', 'fr');
+  });
+
+  it('should save settings between pages', () => {
+    const settings = selectors.getSettings();
+
+    cy.visit('/');
+
+    Promise.all([
+      settings,
+      selectors.getSettings()
+    ]).then(([s, newSettings]) => {
+      expect(s).equal(newSettings);
+    });
+  });
+});
--- jest.preset.js
+++ jest.preset.js
@@ -1,12 +1,13 @@
-const nxPreset = require('@nrwl/jest/preset');
 /* eslint-env node */
+const nxPreset = require('@nrwl/jest/preset');
+
 module.exports = {
-  ...nxPreset,
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
-  transform: {
-    '^.+\\.(ts|js|html)$': 'ts-jest',
-  },
-  resolver: '@nrwl/jest/plugins/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['html', 'lcov', 'text'],
+    ...nxPreset,
+    testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+    transform: {
+        '^.+\\.(ts|js|html)$': 'ts-jest'
+    },
+    resolver: '@nrwl/jest/plugins/resolver',
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageReporters: ['html', 'lcov', 'text']
 };
