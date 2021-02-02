1f4d6e7025b1e6c2e7cd077518da615dd4880943
Alexander Lloyd <axl639@student.bham.ac.uk>
2021-02-02 21:07:57 +0000

    chore: fix formatting in jest config and update deprecated configuration

--- apps/todo/jest.config.js
+++ apps/todo/jest.config.js
@@ -1,24 +1,24 @@
 /* eslint-env node */
 module.exports = {
-  preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-      astTransformers: {
-        before: [
-          'jest-preset-angular/build/InlineFilesTransformer',
-          'jest-preset-angular/build/StripStylesTransformer',
-        ],
-      },
+    preset: '../../jest.preset.js',
+    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+            stringifyContentPathRegex: '\\.(html|svg)$',
+            astTransformers: {
+                before: [
+                    'jest-preset-angular/build/InlineFilesTransformer',
+                    'jest-preset-angular/build/StripStylesTransformer'
+                ]
+            }
+        }
     },
-  },
-  coverageDirectory: '../../coverage/apps/todo',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
-  ],
-  displayName: 'todo',
+    coverageDirectory: '../../coverage/apps/todo',
+    snapshotSerializers: [
+        'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+        'jest-preset-angular/build/AngularSnapshotSerializer.js',
+        'jest-preset-angular/build/HTMLCommentSerializer.js'
+    ],
+    displayName: 'todo'
 };
--- libs/logger/jest.config.js
+++ libs/logger/jest.config.js
@@ -1,13 +1,12 @@
 /* eslint-env node */
-
 module.exports = {
-  preset: '../../jest.preset.js',
-  coverageDirectory: '../../coverage/libs/logger',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
-  ],
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  displayName: 'logger',
+    preset: '../../jest.preset.js',
+    coverageDirectory: '../../coverage/libs/logger',
+    snapshotSerializers: [
+        'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+        'jest-preset-angular/build/AngularSnapshotSerializer.js',
+        'jest-preset-angular/build/HTMLCommentSerializer.js'
+    ],
+    setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+    displayName: 'logger'
 };
