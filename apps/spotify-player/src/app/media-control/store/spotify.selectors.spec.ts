cf98a716221e698e6e82cf1ce7846cc727334788
Alexander Lloyd <4333861+alexander-lloyd@users.noreply.github.com>
2021-05-09 16:14:01 +0100

    feat(spotify-player): refactor spotify selectors

--- apps/spotify-player/src/app/media-control/store/spotify.selectors.spec.ts
+++ apps/spotify-player/src/app/media-control/store/spotify.selectors.spec.ts
@@ -1,13 +1,25 @@
 import * as selectors from './spotify.selectors';
-import {SpotifyState} from './spotify.types';
+import {GlobalState, SpotifyState} from './spotify.types';
 
 describe('Spotify Selectors', () => {
+  it('should get spotify feature state', () => {
+    expect.assertions(1);
+    const state: GlobalState = {
+      spotify: {
+        current: null,
+        playing: false
+      }
+    };
+
+    expect(selectors.getSpotifyFeature(state)).toBe(state.spotify);
+  });
+
   it('should get is not playing', () => {
     expect.assertions(1);
     const state = {
       playing: false
     };
-    expect(selectors.isPlaying.projector(state)).toBe(false);
+    expect(selectors.selectPlaying.projector(state)).toBe(false);
   });
 
   it('should get is playing', () => {
@@ -15,50 +27,67 @@ describe('Spotify Selectors', () => {
     const state = {
       playing: true
     };
-    expect(selectors.isPlaying.projector(state)).toBe(true);
+    expect(selectors.selectPlaying.projector(state)).toBe(true);
   });
 
-  it('should get spotify state', () => {
+  it('should get current song', () => {
     expect.assertions(1);
-    const state = {
-      spotify: {
-        playing: true
-      }
-    } as {spotify: SpotifyState};
-
-    expect(selectors.selectFeature(state)).toBe(state.spotify);
-  });
-
-  it('should get current progress', () => {
-    expect.assertions(1);
-    const state = {
+    const state: SpotifyState = {
+      playing: true,
       current: {
-        progress: 100
+        name: 'Song',
+        progress: 1,
+        total: 2
       }
     };
-
-    expect(selectors.getSongProgress.projector(state)).toBe(state.current.progress);
-  });
-
-  it('should get song length', () => {
-    expect.assertions(1);
-    const state = {
-      current: {
-        total: 100
-      }
-    };
-
-    expect(selectors.getSongLength.projector(state)).toBe(state.current.total);
+    expect(selectors.selectCurrentSong.projector(state)).toBe(state.current);
   });
 
   it('should get song name', () => {
     expect.assertions(1);
     const state = {
-      current: {
-        name: 'Song Name'
-      }
+      name: 'Song Name'
     };
 
-    expect(selectors.getSongName.projector(state)).toBe(state.current.name);
+    expect(selectors.selectSongName.projector(state)).toBe(state.name);
+  });
+
+  it('should return null when no song is playing', () => {
+    expect.assertions(1);
+    const state = null;
+
+    expect(selectors.selectSongName.projector(state)).toBe('');
+  });
+
+  it('should get current progress', () => {
+    expect.assertions(1);
+    const state = {
+      progress: 100
+    };
+
+    expect(selectors.selectSongProgress.projector(state)).toBe(state.progress);
+  });
+
+  it('should return null if no song is playing', () => {
+    expect.assertions(1);
+    const state = null;
+
+    expect(selectors.selectSongProgress.projector(state)).toBe(0);
+  });
+
+  it('should get song length', () => {
+    expect.assertions(1);
+    const state = {
+      total: 100
+    };
+
+    expect(selectors.selectSongLength.projector(state)).toBe(state.total);
+  });
+
+  it('should return null if no current song', () => {
+    expect.assertions(1);
+    const state = null;
+
+    expect(selectors.selectSongLength.projector(state)).toBe(0);
   });
 });
