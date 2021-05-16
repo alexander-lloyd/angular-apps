750e2fd3b2180c372ee34fbab9dea55969798e94
Alexander Lloyd <4333861+alexander-lloyd@users.noreply.github.com>
2021-05-16 11:16:09 +0100

    feat(checkbox): add checkbox component

--- libs/components/checkbox/src/lib/checkbox.module.ts
+++ libs/components/checkbox/src/lib/checkbox.module.ts
@@ -1,10 +1,17 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {CheckboxComponent} from './checkbox.component';
 
 /**
  * Checkbox Module.
  */
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [
+    CheckboxComponent
+  ],
+  exports: [
+    CheckboxComponent
+  ]
 })
 export class CheckboxModule {}
